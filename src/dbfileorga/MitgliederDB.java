d3b95208150b3396baf6116c0dc619a9fc299db2
Simon <53291985+SiG268@users.noreply.github.com>
2021-02-05 13:54:49 +0100

    init

---
+++ src/dbfileorga/MitgliederDB.java
@@ -0,0 +1,260 @@
+package dbfileorga;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MitgliederDB implements Iterable<Record> {
+
+    protected DBBlock db[] = new DBBlock[8];
+
+
+    public MitgliederDB(boolean ordered) {
+        this();
+        insertMitgliederIntoDB(ordered);
+
+    }
+
+    public MitgliederDB() {
+        initDB();
+    }
+
+    private void initDB() {
+        for (int i = 0; i < db.length; ++i) {
+            db[i] = new DBBlock();
+        }
+
+    }
+
+    private void insertMitgliederIntoDB(boolean ordered) {
+        MitgliederTableAsArray mitglieder = new MitgliederTableAsArray();
+        String mitgliederDatasets[];
+        if (ordered) {
+            mitgliederDatasets = mitglieder.recordsOrdered;
+        } else {
+            mitgliederDatasets = mitglieder.records;
+        }
+        for (String currRecord : mitgliederDatasets) {
+            appendRecord(new Record(currRecord));
+        }
+    }
+
+
+    protected int appendRecord(Record record) {
+        //search for block where the record should be appended
+        int currBlock = getBlockNumOfRecord(getNumberOfRecords());
+        int result = db[currBlock].insertRecordAtTheEnd(record);
+        if (result != -1) { //insert was successful
+            return result;
+        } else if (currBlock < db.length) { // overflow => insert the record into the next block
+            return db[currBlock + 1].insertRecordAtTheEnd(record);
+        }
+        return -1;
+    }
+
+
+    @Override
+    public String toString() {
+        String result = new String();
+        for (int i = 0; i < db.length; ++i) {
+            result += "Block " + i + "\n";
+            result += db[i].toString();
+            result += "-------------------------------------------------------------------------------------\n";
+        }
+        return result;
+    }
+
+    /**
+     * Returns the number of Records in the Database
+     *
+     * @return number of records stored in the database
+     */
+    public int getNumberOfRecords() {
+        int result = 0;
+        for (DBBlock currBlock : db) {
+            result += currBlock.getNumberOfRecords();
+        }
+        return result;
+    }
+
+    /**
+     * Returns the block number of the given record number
+     *
+     * @param recNum the record number to search for
+     * @return the block number or -1 if record is not found
+     */
+    public int getBlockNumOfRecord(int recNum) {
+        int recCounter = 0;
+        for (int i = 0; i < db.length; ++i) {
+            if (recNum <= (recCounter + db[i].getNumberOfRecords())) {
+                return i;
+            } else {
+                recCounter += db[i].getNumberOfRecords();
+            }
+        }
+        return -1;
+    }
+
+    public DBBlock getBlock(int i) {
+        return db[i];
+    }
+
+
+    /**
+     * Returns the record matching the record number
+     *
+     * @param recNum the term to search for
+     * @return the record matching the search term
+     */
+    public Record read(int recNum) {
+        //TODO implement
+        int blockNumber = this.getBlockNumOfRecord(recNum);
+        if (blockNumber != -1) {
+            DBBlock block = this.getBlock(blockNumber);
+            if (block != null) {
+                int counter = 0;
+                for (int i = 0; i < blockNumber; i++) {
+                    counter = counter + getBlock(i).getNumberOfRecords();
+                }
+                return block.getRecord(recNum - counter);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns the number of the first record that matches the search term
+     *
+     * @param searchTerm the term to search for
+     * @return the number of the record in the DB -1 if not found
+     */
+    public int findPos(String searchTerm) {
+        //TODO implement
+        for (int i = 1; i <= this.getNumberOfRecords(); i++) {
+            if ((read(i).getAttribute(1)).equals(searchTerm)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Inserts the record into the file and returns the record number
+     *
+     * @param record
+     * @return the record number of the inserted record
+     */
+    public int insert(Record record) {
+        int res = this.appendRecord(record);
+        if (res != -1) {
+            return this.getNumberOfRecords();
+        }
+        return -1;
+    }
+
+
+    private int getFirstRecordNumberFormBlock(int blockNum) {
+            DBBlock block = this.getBlock(blockNum);
+            if (block != null) {
+                int counter = 0;
+                for (int i = 0; i < blockNum; i++) {
+                    counter = counter + getBlock(i).getNumberOfRecords();
+                }
+                return counter+1;
+            }
+        return -1;
+    }
+
+
+    /**
+     * Deletes the record specified
+     *
+     * @param numRecord number of the record to be deleted
+     */
+    public void delete(int numRecord) {
+        ArrayList<Record> records = new ArrayList<>();
+        int blockNumber = getBlockNumOfRecord(numRecord);
+        int firstBlockNumber = getFirstRecordNumberFormBlock(getBlockNumOfRecord(numRecord));
+        for (int i = firstBlockNumber; i <= getNumberOfRecords(); i++) {
+            if(i!=numRecord) {
+                records.add(read(i));
+            }
+        }
+        for(int i=blockNumber; i<db.length;i++){
+            db[i]=new DBBlock();
+        }
+        for(Record r: records){
+            this.appendRecord(r);
+        }
+    }
+
+
+
+
+
+    /**
+     * Replaces the record at the specified position with the given one.
+     *
+     * @param numRecord the position of the old record in the db
+     * @param record    the new record
+     */
+    public void modify(int numRecord, Record record) {
+        ArrayList<Record> records = new ArrayList<>();
+        int blockNumber = getBlockNumOfRecord(numRecord);
+        int firstBlockNumber = getFirstRecordNumberFormBlock(getBlockNumOfRecord(numRecord));
+        for (int i = firstBlockNumber; i <= getNumberOfRecords(); i++) {
+            if(i!=numRecord) {
+                records.add(read(i));
+            }
+            else{
+                records.add(record);
+            }
+        }
+        for(int i=blockNumber; i<db.length;i++){
+            db[i]=new DBBlock();
+        }
+        for(Record r: records){
+            this.appendRecord(r);
+        }
+    }
+
+
+    @Override
+    public Iterator<Record> iterator() {
+        return new DBIterator();
+    }
+
+    private class DBIterator implements Iterator<Record> {
+
+        private int currBlock = 0;
+        private Iterator<Record> currBlockIter = db[currBlock].iterator();
+
+        public boolean hasNext() {
+            if (currBlockIter.hasNext()) {
+                return true;
+            } else if (currBlock < db.length) { //continue search in the next block
+                return db[currBlock + 1].iterator().hasNext();
+            } else {
+                return false;
+            }
+        }
+
+        public Record next() {
+            if (currBlockIter.hasNext()) {
+                return currBlockIter.next();
+            } else if (currBlock < db.length) { //continue search in the next block
+                currBlockIter = db[++currBlock].iterator();
+                return currBlockIter.next();
+            } else {
+                throw new NoSuchElementException();
+            }
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+
+}
